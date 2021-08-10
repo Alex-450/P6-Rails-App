e3bfef723faf2c5b565c51a4827c442bfd562619
Alex Stearn <alex.wmstearn@gmail.com>
2021-08-10 19:50:53 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActivityType.create([
+  {
+    name: 'running'
+  },
+  {
+    name: 'cycling'
+  },
+  {
+    name: 'walking'
+  },
+  {
+    name: 'swimming'
+  }
+  ])
